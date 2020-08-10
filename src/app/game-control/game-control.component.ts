a18b583927510bcb83e56d9840eb3b967cd3d6f6
tenor1 <tenor@ukr.net>
2020-08-10 12:01:41 +0300

    add components GameControl, Odd, Even

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.less']
+})
+export class GameControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
